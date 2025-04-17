46a61d51593b81b4cc5f7e344c796dbb93ad9ecf
Vinamra Sharma <vinamrasharma0523@gmai.com>
2025-04-18 04:45:54 +0530

    footer updation underline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,78 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
+  const baseClass = `
+    relative text-md font-medium px-1 transition duration-300
+    after:content-[''] after:absolute after:left-0 after:-bottom-1
+    after:h-[2px] after:bg-indigo-500 after:transition-all after:duration-300
+    after:w-0 hover:after:w-full
+  `;
+
+  const activeClass = `
+    text-indigo-500 after:w-full
+  `;
+
   return (
     <footer className="bg-base-300 text-base-content p-4 text-center shadow-md w-full mt-auto">
-      <div className="flex flex-wrap justify-center gap-4 items-center mb-2">
-    <Link to="/privacy-policy" className="text-md font-medium hover:opacity-80 transition duration-300">
-      Privacy Policy
-    </Link>
-    <Link to="/terms" className="text-md font-medium hover:opacity-80 transition duration-300">
-      Terms & Conditions
-    </Link>
-    <Link to="/about" className="text-md font-medium hover:opacity-80 transition duration-300">
-      About
-    </Link>
-  </div>
+      <div className="flex flex-wrap justify-center gap-4 items-center mb-3">
+        <NavLink
+          to="/privacy-policy"
+          className={({ isActive }) =>
+            `${baseClass} ${isActive ? activeClass : ""}`
+          }
+        >
+          Privacy Policy
+        </NavLink>
+
+        <NavLink
+          to="/terms"
+          className={({ isActive }) =>
+            `${baseClass} ${isActive ? activeClass : ""}`
+          }
+        >
+          Terms & Conditions
+        </NavLink>
+
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `${baseClass} ${isActive ? activeClass : ""}`
+          }
+        >
+          About
+        </NavLink>
+      </div>
 
       <div className="flex flex-col items-center gap-2">
-        
-       
         <div className="flex gap-3">
-          <a href="https://github.com/Vinamra05" target="_blank" className="hover:opacity-80 transition duration-300">
+          <a
+            href="https://github.com/Vinamra05"
+            target="_blank"
+            className="hover:opacity-80 transition duration-300"
+          >
             <img
               src="https://img.icons8.com/ios-glyphs/30/000000/github.png"
               alt="GitHub"
               className="h-7 w-7 filter dark:invert"
             />
           </a>
-          <a href="https://www.linkedin.com/in/vinamra-sharma-84981a289/" target="_blank" className="hover:opacity-80 transition duration-300">
+          <a
+            href="https://www.linkedin.com/in/vinamra-sharma-84981a289/"
+            target="_blank"
+            className="hover:opacity-80 transition duration-300"
+          >
             <img
               src="https://img.icons8.com/ios-glyphs/30/000000/linkedin.png"
               alt="LinkedIn"
-              className="h-7 w-7 filter  dark:invert"
+              className="h-7 w-7 filter dark:invert"
             />
           </a>
-          <a href="https://x.com/VinamraSharma05"  target="_blank"  className="hover:opacity-80 transition duration-300">
+          <a
+            href="https://x.com/VinamraSharma05"
+            target="_blank"
+            className="hover:opacity-80 transition duration-300"
+          >
             <img
               src="https://img.icons8.com/ios-glyphs/30/000000/twitter.png"
               alt="Twitter"
@@ -43,11 +81,9 @@ const Footer = () => {
           </a>
         </div>
 
-       
         <p className="text-[16px] opacity-70 transition duration-300">
           techTinder © {new Date().getFullYear()} Made with ❤️ by Vinamra.
         </p>
-            
       </div>
     </footer>
   );
